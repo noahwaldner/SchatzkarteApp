4d691b41388625b4e8743d7bcfb2d0f6b7c96cf3
noahwaldner <noah@noahwaldner.ch>
2016-10-17 10:30:50 +0200

    Initial commit

---
+++ app/src/main/java/com/example/user/schatzkarteapp/MainActivity.java
@@ -0,0 +1,62 @@
+package com.example.user.schatzkarteapp;
+
+import android.app.Activity;
+import android.graphics.Color;
+import android.os.Environment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import org.osmdroid.api.IMapController;
+import org.osmdroid.tileprovider.MapTileProviderArray;
+import org.osmdroid.tileprovider.MapTileProviderBase;
+import org.osmdroid.tileprovider.modules.IArchiveFile;
+import org.osmdroid.tileprovider.modules.MBTilesFileArchive;
+import org.osmdroid.tileprovider.modules.MapTileFileArchiveProvider;
+import org.osmdroid.tileprovider.modules.MapTileModuleProviderBase;
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.tileprovider.tilesource.XYTileSource;
+import org.osmdroid.tileprovider.util.SimpleRegisterReceiver;
+import org.osmdroid.views.MapView;
+import org.osmdroid.views.overlay.TilesOverlay;
+
+import java.io.File;
+
+public class MainActivity extends Activity {
+    @Override public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //important! set your user agent to prevent getting banned from the osm servers
+        org.osmdroid.tileprovider.constants.OpenStreetMapTileProviderConstants.setUserAgentValue(BuildConfig.APPLICATION_ID);
+
+        MapView map = (MapView) findViewById(R.id.map);
+        map.setTileSource(TileSourceFactory.MAPNIK);
+
+        map.setMultiTouchControls(true);
+        map.setBuiltInZoomControls(true);
+
+        IMapController controller = map.getController();
+        controller.setZoom(18);
+
+// Die TileSource beschreibt die Eigenschaften der Kacheln die wir anzeigen
+        XYTileSource treasureMapTileSource = new XYTileSource("mbtiles", 1, 20, 256, ".png", "http://example.org/");
+
+        File file = new File(Environment.getExternalStorageDirectory() /* entspricht /sdcard/ */, "hsr.mbtiles");
+
+/* Das verwenden von mbtiles ist leider ein wenig aufwändig, wir müssen
+ * unsere XYTileSource in verschiedene Klassen 'verpacken' um sie dann
+ * als TilesOverlay über der Grundkarte anzuzeigen.
+ */
+        MapTileModuleProviderBase treasureMapModuleProvider = new MapTileFileArchiveProvider(new SimpleRegisterReceiver(this),
+                treasureMapTileSource, new IArchiveFile[] { MBTilesFileArchive.getDatabaseFileArchive(file) });
+
+        MapTileProviderBase treasureMapProvider = new MapTileProviderArray(treasureMapTileSource, null,
+                new MapTileModuleProviderBase[] { treasureMapModuleProvider });
+
+        TilesOverlay treasureMapTilesOverlay = new TilesOverlay(treasureMapProvider, getBaseContext());
+        treasureMapTilesOverlay.setLoadingBackgroundColor(Color.TRANSPARENT);
+
+// Jetzt können wir den Overlay zu unserer Karte hinzufügen:
+        map.getOverlays().add(treasureMapTilesOverlay);
+
+    }
+}
